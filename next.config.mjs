7ca2febd42efcb784d6b4a51cdbc96b036dfc7fb
Ethan Russell <44298283+Ethan-Russell@users.noreply.github.com>
2023-02-28 07:27:48 -0500

    Update prefixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
+const repo = 'test-blog'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 /**
    * @type {import('next').NextConfig}
    */
 const nextConfig   = {
   images: {
     loader: 'akamai',
-    path: '.',
+    path: './',
   },
-  assetPrefix: './',
+  assetPrefix: assetPrefix,
+  basePath: basePath
 };
 
 export default   nextConfig;
